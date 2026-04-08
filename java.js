bb5586880b2b1e4eed3ee4143b0766032db0c56f
Priyesh Choubey <priyesh@Priyeshs-MacBook-Pro.local>
2026-04-08 11:48:10 +0530

    fixed the bug added new dark mode and searching

--- java.js
+++ java.js
@@ -101,23 +101,58 @@ function searchVehicle() {
 }
 
 
+function searchVehicle() {
+
+    let input = Number(document.getElementById("search").value);
+    let results = document.getElementById("results");
+
+    results.innerHTML = "";
+
+    if (!input) {
+        alert("Enter Make ID");
+        return;
+    }
+
+    fetch("https://vpic.nhtsa.dot.gov/api/vehicles/GetMakesForManufacturerAndYear/mer?year=2014&format=json")
+        .then(response => response.json())
+        .then(data => {
+
+            let filtered = data.Results.filter(car => 
+                car.MakeId === input
+            );
+
+            if (filtered.length === 0) {
+                results.innerHTML = "<p>No match found</p>";
+                return;
+            }
+
+            let html = filtered.map(car => `
+                <div class="card">
+                    <h3>${car.MakeName}</h3>
+                    <p>MfrId: ${car.MfrId}</p>
+                    <p>MfrName: ${car.MfrName}</p>
+                </div>
+            `).join("");
+
+            results.innerHTML = html;
+
+        })
+        .catch(error => {
+            console.log(error);
+            results.innerHTML = "<p>Error fetching data</p>";
+        });
+}
+
+
 
 let toggleBtn = document.getElementById("themeToggle");
-
-// Load saved theme
-if (localStorage.getItem("theme") === "dark") {
-    document.body.classList.add("dark");
-    toggleBtn.textContent = "☀️";
-}
 
 toggleBtn.addEventListener("click", () => {
     document.body.classList.toggle("dark");
 
     if (document.body.classList.contains("dark")) {
-        localStorage.setItem("theme", "dark");
         toggleBtn.textContent = "☀️";
     } else {
-        localStorage.setItem("theme", "light");
         toggleBtn.textContent = "🌙";
     }
 });
